8489bd411ada67b12078dd7d094017c1b72f1b1e
Linneeeeeeee <lindsay.gelle@gmail.com>
2024-04-17 16:00:26 +1000

    Update aws_sqs_queue.tf

--- aws_sqs_queue.tf
+++ aws_sqs_queue.tf
@@ -1,7 +1,7 @@
 resource "aws_sqs_queue" "s3_object_created_text" {
   content_based_deduplication = true
   fifo_queue                  = true
-  name                        = "s3_object_created_text"
+  name                        = "${local.organization}-s3-object-created-text"
   tags = {
     account_arn  = data.aws_caller_identity.main.arn
     account_id   = data.aws_caller_identity.main.account_id
