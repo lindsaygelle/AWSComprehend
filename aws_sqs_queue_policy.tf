7bec37bfbce73dcc851f002262fcc2a3f63bf442
Linneeeeeeee <lindsay.gelle@gmail.com>
2024-04-17 16:06:53 +1000

    Update aws_sqs_queue_policy.tf

--- aws_sqs_queue_policy.tf
+++ aws_sqs_queue_policy.tf
@@ -1,4 +1,4 @@
-resource "aws_sqs_queue_policy" "s3_object_created" {
+resource "aws_sqs_queue_policy" "s3_object_created_text" {
   policy    = data.aws_iam_policy_document.sqs_queue.json
-  queue_url = aws_sqs_queue.s3_object_created.id
+  queue_url = aws_sqs_queue.s3_object_created_text.id
 }
