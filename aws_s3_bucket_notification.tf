7143927b62056d673c9a9b91124942a1ccde5ddd
Linneeeeeeee <lindsay.gelle@gmail.com>
2024-04-17 15:44:55 +1000

    Update aws_s3_bucket_notification.tf

--- aws_s3_bucket_notification.tf
+++ aws_s3_bucket_notification.tf
@@ -1,3 +1,10 @@
 resource "aws_s3_bucket_notification" "main" {
   bucket = aws_s3_bucket.main.id
+
+  queue {
+    id            = aws_sqs_queue.s3_object_created_text.id
+    events        = ["s3:ObjectCreated:*"]
+    filter_prefix = aws_s3_object.text.key
+    queue_arn     = aws_sqs_queue.s3_object_created_text.arn
+  }
 }
