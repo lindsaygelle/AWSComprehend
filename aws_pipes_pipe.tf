bb451026f89e0fbe5ba9ad82e8d372241987ba3a
Linneeeeeeee <lindsay.gelle@gmail.com>
2024-04-26 12:43:02 +1000

    Dev (#9)
    * Dev

--- aws_pipes_pipe.tf
+++ aws_pipes_pipe.tf
@@ -1,33 +1,3 @@
-resource "aws_pipes_pipe" "sfn_state_machine_s3_object_created_detect_entities" {
-  depends_on = [
-    aws_iam_role_policy.pipes_pipe_s3_object_created_detect_entities
-  ]
-  name     = aws_sfn_state_machine.comprehend_detect_entities.name
-  role_arn = aws_iam_role.pipes_pipe_s3_object_created_detect_entities.arn
-  source   = aws_sqs_queue.s3_object_created_detect_entities.arn
-  target   = aws_sfn_state_machine.comprehend_detect_entities.arn
-  target_parameters {
-    step_function_state_machine_parameters {
-      invocation_type = "FIRE_AND_FORGET"
-    }
-  }
-}
-
-resource "aws_pipes_pipe" "sfn_state_machine_s3_object_created_document" {
-  depends_on = [
-    aws_iam_role_policy.pipes_pipe_s3_object_created_document
-  ]
-  name     = aws_sfn_state_machine.comprehend_detect_dominant_language.name
-  role_arn = aws_iam_role.pipes_pipe_s3_object_created_document.arn
-  source   = aws_sqs_queue.s3_object_created_document.arn
-  target   = aws_sfn_state_machine.comprehend_detect_dominant_language.arn
-  target_parameters {
-    step_function_state_machine_parameters {
-      invocation_type = "FIRE_AND_FORGET"
-    }
-  }
-}
-
 resource "aws_pipes_pipe" "sfn_state_machine_s3_object_created_text" {
   depends_on = [
     aws_iam_role_policy.pipes_pipe_s3_object_created_text
