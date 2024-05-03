fbba2f10aae24e33076e46a6e551fd1ed712e9b5
Linneeeeeeee <lindsay.gelle@gmail.com>
2024-05-03 10:07:47 +1000

    Dev (#23)

--- aws_glue_catalog_database.tf
+++ aws_glue_catalog_database.tf
@@ -1,3 +1,12 @@
 resource "aws_glue_catalog_database" "datalake" {
   name = "${local.organization}-datalake"
+  tags = {
+    account_arn    = data.aws_caller_identity.main.arn
+    account_id     = data.aws_caller_identity.main.account_id
+    organization   = local.organization
+    region         = data.aws_region.main.name
+    s3_bucket      = aws_s3_bucket.main.name
+    s3_target_path = "s3://${aws_s3_bucket.main.id}/${aws_s3_object.datalake.key}"
+    workspace      = terraform.workspace
+  }
 }
--- aws_glue_crawler.tf
+++ aws_glue_crawler.tf
@@ -6,4 +6,13 @@ resource "aws_glue_crawler" "datalake" {
   s3_target {
     path = "s3://${aws_s3_bucket.main.id}/${aws_s3_object.datalake.key}"
   }
+  tags = {
+    account_arn    = data.aws_caller_identity.main.arn
+    account_id     = data.aws_caller_identity.main.account_id
+    organization   = local.organization
+    region         = data.aws_region.main.name
+    s3_bucket      = aws_s3_bucket.main.name
+    s3_target_path = "s3://${aws_s3_bucket.main.id}/${aws_s3_object.datalake.key}"
+    workspace      = terraform.workspace
+  }
 }
